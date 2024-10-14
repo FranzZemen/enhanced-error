f98d9d6deb3650769d80756542cc2c687a03afa6
Franz Zemen <franzzemen@gmail.com>
2024-10-14 07:34:24 -0400

    pre-version change: bugfix

--- src/project/index.ts
+++ src/project/index.ts
@@ -8,56 +8,14 @@ import {LoggerAdapter} from '@franzzemen/logger-adapter';
 export class EnhancedError extends Error {
   isOriginalError = true;
 
-  constructor(protected err: string | Error, public isLogged = false) {
-    super(typeof err === 'string' ? err : err.message);
-    this.isOriginalError = typeof err === 'object';
-  }
 
-  override toString(): string {
-    if (this.err) {
-      return this.err.toString();
+  constructor(error?: Error, protected msg: string = '', public isLogged: boolean = false) {
+    super(msg.length > 0 ? msg : error?.message);
+    if(error) {
+      this.cause = error;
+      this.isOriginalError = false;
     } else {
-      return super.toString();
-    }
-  }
-
-  override toLocaleString(): string {
-    if (this.err) {
-      return this.err.toLocaleString();
-    } else {
-      return super.toLocaleString();
-    }
-  }
-
-  override valueOf(): Object {
-    if (this.err) {
-      return this.err.valueOf();
-    } else {
-      return super.valueOf();
-    }
-  }
-
-  override hasOwnProperty(v: PropertyKey): boolean {
-    if (this.err) {
-      return this.err.hasOwnProperty(v);
-    } else {
-      return super.hasOwnProperty(v);
-    }
-  }
-
-  override isPrototypeOf(v: Object): boolean {
-    if (this.err) {
-      return this.err.isPrototypeOf(v);
-    } else {
-      return super.isPrototypeOf(v);
-    }
-  }
-
-  override propertyIsEnumerable(v: PropertyKey): boolean {
-    if (this.err) {
-      return this.err.propertyIsEnumerable(v);
-    } else {
-      return super.propertyIsEnumerable(v);
+      this.isOriginalError = true;
     }
   }
 }
@@ -68,14 +26,14 @@ export class EnhancedError extends Error {
  * @param error
  * @param log
  */
-export function logAndEnhanceError(error: EnhancedError | Error | string, log: LoggerAdapter): EnhancedError {
+export function logAndEnhanceError(log: LoggerAdapter, error?: Error, message?: string): EnhancedError {
   let err = error as EnhancedError;
   if (!(error instanceof EnhancedError)) {
-    err = new EnhancedError(error);
+    err = new EnhancedError(error, message, false);
   }
   if (!err.isLogged) {
-    err.isLogged = true;
     log.error(err);
+    err.isLogged = true;
   }
   return err;
 }
