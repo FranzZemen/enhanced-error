6f802726428409e42be4b29d5a83f58d5ae73d52
Franz Zemen <franzzemen@gmail.com>
2024-10-26 12:19:40 -0400

    pre-version change: rewrite

--- src/project/index.ts
+++ src/project/index.ts
@@ -23,18 +23,52 @@ export class EnhancedError extends Error {
 /**
  * Create an enhanced error and log it, marking it as logged.  If the error is already enhanced, it will not log again.
  * This avoids the forever logs on error.
- * @param error
- * @param log
+ *
+ * If error is an EnhancedError it will be thrown.  It will be logged as is if not logged.  If message is provided, it will be logged as a warning first.
+ * If error is a string, it is used to construct a new EnhancedError, which is logged.  If message is provided, it will be logged as a warning
+ * If error is neither a string nor an EnhancedError, a new EnhancedError is constructed using error as cause and the message.
+ * If error is not provided, the message will be  used to create a new EnhancedError
+ *
+ * @param log The logger to use, required.
+ * @param error An EnhancedError, Error, unknown or string, or undefined required
+ * @param message A string, optional
  */
-export function logAndEnhanceError(log: LoggerAdapter, error?: unknown, message?: string): EnhancedError {
-  let enhancedError = error as EnhancedError;
-  if (!(error instanceof EnhancedError)) {
-    enhancedError = new EnhancedError(error, message, false);
-  }
-  if (!enhancedError.isLogged) {
+export function logAndEnhanceError(log: LoggerAdapter, error?: string | Error | unknown, message?: string): EnhancedError {
+  if(error !== undefined) {
+    if(error instanceof EnhancedError) {
+      const enhanceError = error as EnhancedError;
+      if(message) {
+        log.warn(message);
+      }
+      if(enhanceError.isLogged === false) {
+        log.error(enhanceError);
+        enhanceError.isLogged = true;
+      }
+      return enhanceError;
+    } else if (typeof error === 'string') {
+      const enhancedError = new EnhancedError(undefined, error);
+      if(message) {
+        log.warn(message);
+      }
+      log.error(enhancedError);
+      enhancedError.isLogged = true;
+      return enhancedError;
+    } else {
+      const enhancedError = new EnhancedError(error, message);
+      log.error(enhancedError);
+      enhancedError.isLogged = true;
+      return enhancedError;
+    }
+  } else if(message) {
+    const enhancedError = new EnhancedError(undefined, message);
     log.error(enhancedError);
     enhancedError.isLogged = true;
+    return enhancedError
+  } else {
+    const enhancedError = new EnhancedError(undefined, 'Control Feature: unknown');
+    log.error(enhancedError);
+    enhancedError.isLogged = true;
+    return enhancedError;
   }
-  return enhancedError;
 }
 
